4bf8a9d9ddd9f6cbf1ad9748de5aa2c19da59da1
Angeline B <bedisad@students.nu-moa.edu.ph>
2024-06-01 16:10:32 +0800

    Added Delete Function: Price Rules

--- app/src/main/java/com/example/nuradadmin/Activities/List_and_Create/Activity_PriceRules.java
+++ app/src/main/java/com/example/nuradadmin/Activities/List_and_Create/Activity_PriceRules.java
@@ -1,9 +1,15 @@
 package com.example.nuradadmin.Activities.List_and_Create;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -29,14 +35,19 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Activity_PriceRules extends AppCompatActivity {
-    private ImageView back_icon;
-    private TextView title;
+public class Activity_PriceRules extends AppCompatActivity implements Adapter_PriceRule.OnLongItemClickListener, Adapter_PriceRule.OnSelectionChangedListener{
+    public static boolean isContextualModeEnabled = false;
+    private ImageView back_icon, moveOutDeleteMode, delete_icon;
+    private TextView title, itemCounter;
     private RecyclerView recyclerView;
     private List<Model_PriceRule> modelPriceRulesList;
     private DatabaseReference priceRules_DBref;
     private ValueEventListener eventListener;
     private FloatingActionButton floatingBtn;
+    private Adapter_PriceRule adapter;
+    private View toolbar_normal;
+    private View toolbar_deleteMode;
+    @SuppressLint("NotifyDataSetChanged")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,9 +61,14 @@ public class Activity_PriceRules extends AppCompatActivity {
         SystemUIUtil.setupSystemUI(this);
 
         back_icon = findViewById(R.id.back_icon);
+        moveOutDeleteMode = findViewById(R.id.back_btn);
         title = findViewById(R.id.title);
         floatingBtn = findViewById(R.id.floatingActionButton);
         recyclerView = findViewById(R.id.recyclerView);
+        toolbar_normal = findViewById(R.id.toolbar_normal);
+        toolbar_deleteMode = findViewById(R.id.toolbar_deleteMode);
+        itemCounter = findViewById(R.id.counter);
+        delete_icon = findViewById(R.id.delete_icon);
 
         title.setText("Price Rules");
 
@@ -60,7 +76,7 @@ public class Activity_PriceRules extends AppCompatActivity {
         recyclerView.setLayoutManager(gridLayoutManager);
 
         modelPriceRulesList = new ArrayList<>();
-        Adapter_PriceRule adapter = new Adapter_PriceRule(Activity_PriceRules.this, modelPriceRulesList);
+        adapter = new Adapter_PriceRule(Activity_PriceRules.this, modelPriceRulesList, this, this);
         recyclerView.setAdapter(adapter);
 
         priceRules_DBref = FirebaseDatabase.getInstance().getReference("Price Rules");
@@ -90,10 +106,96 @@ public class Activity_PriceRules extends AppCompatActivity {
             finish();
         });
 
+        moveOutDeleteMode.setOnClickListener(view -> {
+            isContextualModeEnabled = false;
+            toolbar_deleteMode.setVisibility(View.GONE);
+            toolbar_normal.setVisibility(View.VISIBLE);
+            adapter.clearSelectedItems(); // Clear selected items
+            adapter.notifyDataSetChanged();
+            updateItemCounter(0);
+        });
+
+        delete_icon.setOnClickListener(view -> {
+            List<Model_PriceRule> selectedItems = adapter.getSelectedItems();
+            if (selectedItems.isEmpty()) {
+                Toast.makeText(this, "No item selected", Toast.LENGTH_SHORT).show();
+            } else {
+                showDeleteItemDialog(this);
+            }
+        });
+
         floatingBtn.setOnClickListener(view ->{
             Intent i = new Intent(this, Activity_CreatePriceRules.class);
             startActivity(i);
             finish();
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (priceRules_DBref != null && eventListener != null) {
+            priceRules_DBref.removeEventListener(eventListener);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        isContextualModeEnabled = false;
+        if (adapter != null) {
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public void onLongItemClick(int position) {
+        if (!adapter.getSelectedItems().contains(position)) {
+            isContextualModeEnabled = true;
+            updateItemCounter(1);
+            toolbar_normal.setVisibility(View.GONE);
+            toolbar_deleteMode.setVisibility(View.VISIBLE);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public void onSelectionChanged(int count) {
+        updateItemCounter(count);
+    }
+
+    private void updateItemCounter(int count) {
+        itemCounter.setText(count + " Item(s) Selected");
+    }
+
+    private void showDeleteItemDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Delete Item");
+        builder.setMessage("Are you sure you want to delete the selected item(s)?");
+
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Delete selected item(s)
+                for (Model_PriceRule priceRule : adapter.getSelectedItems()) {
+                    priceRules_DBref.child(priceRule.getRuleName()).removeValue();
+                }
+                // Reset contextual mode and update UI
+                isContextualModeEnabled = false;
+                toolbar_deleteMode.setVisibility(View.GONE);
+                toolbar_normal.setVisibility(View.VISIBLE);
+                adapter.clearSelectedItems();
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        builder.create().show();
     }
 }
--- app/src/main/java/com/example/nuradadmin/Adapters/Adapter_PriceRule.java
+++ app/src/main/java/com/example/nuradadmin/Adapters/Adapter_PriceRule.java
@@ -2,39 +2,56 @@ package com.example.nuradadmin.Adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.nuradadmin.Activities.List_and_Create.Activity_CreatePriceRules;
+import com.example.nuradadmin.Activities.List_and_Create.Activity_PriceRules;
+import com.example.nuradadmin.Activities.List_and_Create.Activity_RoomTypes;
 import com.example.nuradadmin.Models.Model_PriceRule;
+import com.example.nuradadmin.Models.Model_RoomType;
 import com.example.nuradadmin.R;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 public class Adapter_PriceRule extends RecyclerView.Adapter<Adapter_PriceRule.MyViewHolder> {
     private Context context;
     private List<Model_PriceRule> modelPriceRulesList;
+    private Adapter_PriceRule.OnLongItemClickListener longItemClickListener;
+    private Adapter_PriceRule.OnSelectionChangedListener selectionChangedListener;
+    private List<Integer> selectedItems = new ArrayList<>();
 
-    public Adapter_PriceRule(Context context, List<Model_PriceRule> modelPriceRulesList) {
+    public Adapter_PriceRule(@NonNull Context context, List<Model_PriceRule> modelPriceRulesList,
+                             Adapter_PriceRule.OnLongItemClickListener longItemClickListener,
+                             Adapter_PriceRule.OnSelectionChangedListener selectionChangedListener) {
         this.context = context;
         this.modelPriceRulesList = modelPriceRulesList;
+        this.longItemClickListener = longItemClickListener;
+        this.selectionChangedListener = selectionChangedListener;
     }
 
     @NonNull
     @Override
     public Adapter_PriceRule.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_pricerule, parent, false);
-        return new Adapter_PriceRule.MyViewHolder(view);
+        return new Adapter_PriceRule.MyViewHolder(view, longItemClickListener);
+
+
     }
 
     @Override
@@ -42,9 +59,68 @@ public class Adapter_PriceRule extends RecyclerView.Adapter<Adapter_PriceRule.My
         Model_PriceRule modelPriceRule = modelPriceRulesList.get(position);
         holder.priceRule.setText(modelPriceRule.getRuleName());
 
+        holder.checkbox.setOnCheckedChangeListener(null); // Clear previous listener
+
+        if (Activity_PriceRules.isContextualModeEnabled && selectedItems.contains(position)) {
+            holder.checkbox.setChecked(true);
+            holder.background.setBackgroundColor(Color.parseColor("#FBE3F3"));
+        } else {
+            holder.checkbox.setChecked(false);
+            holder.background.setBackgroundColor(ContextCompat.getColor(context, R.color.white));
+        }
+
         // Format the Price
         DecimalFormat decimalFormat = new DecimalFormat("#0.00");
         holder.price.setText(decimalFormat.format(modelPriceRule.getPrice()));
+
+        if (!Activity_PriceRules.isContextualModeEnabled) {
+            // CONTEXTUAL MODE: FALSE
+            holder.checkbox.setVisibility(View.GONE);
+            holder.background.setOnClickListener(v -> {
+                holder.background.setBackgroundColor(ContextCompat.getColor(context, R.color.white));
+            });
+
+            holder.background.setOnLongClickListener(v -> {
+                if (!selectedItems.contains(position)) {
+                    // Add the item to selectedItems only if it's not already selected
+                    selectedItems.add(position);
+                    selectionChangedListener.onSelectionChanged(selectedItems.size());
+                }
+                Activity_PriceRules.isContextualModeEnabled = true;
+                longItemClickListener.onLongItemClick(position);
+                notifyDataSetChanged();
+                return true;
+            });
+        } else {
+            // CONTEXTUAL MODE: TRUE
+            holder.checkbox.setVisibility(View.VISIBLE);
+            holder.background.setOnClickListener(v -> {
+                if (holder.checkbox.isChecked()) {
+                    selectedItems.remove((Integer) position);
+                } else {
+                    selectedItems.add(position);
+                }
+                selectionChangedListener.onSelectionChanged(selectedItems.size());
+                notifyDataSetChanged();
+            });
+
+            holder.background.setOnLongClickListener(v -> {
+                holder.background.setBackgroundColor(ContextCompat.getColor(context, R.color.white));
+                holder.checkbox.setChecked(false);
+                return false;
+            });
+        }
+
+        holder.checkbox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                selectedItems.add(position);
+                holder.background.setBackgroundColor(Color.parseColor("#FBE3F3"));
+            } else {
+                selectedItems.remove((Integer) position);
+                holder.background.setBackgroundColor(ContextCompat.getColor(context, R.color.white));
+            }
+            selectionChangedListener.onSelectionChanged(selectedItems.size());
+        });
 
         holder.edit_btn.setOnClickListener(v -> {
             Intent intent = new Intent(context, Activity_CreatePriceRules.class);
@@ -65,14 +141,47 @@ public class Adapter_PriceRule extends RecyclerView.Adapter<Adapter_PriceRule.My
         return modelPriceRulesList.size();
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
+    public List<Model_PriceRule> getSelectedItems() {
+        List<Model_PriceRule> selectedPriceRules = new ArrayList<>();
+        for (int i : selectedItems) {
+            selectedPriceRules.add(modelPriceRulesList.get(i));
+        }
+        return selectedPriceRules;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener{
+        CheckBox checkbox;
+        RelativeLayout background;
         TextView priceRule, price;
         Button edit_btn;
-        public MyViewHolder(@NonNull View itemView) {
+        OnLongItemClickListener longItemClickListener;
+        public MyViewHolder(@NonNull View itemView, Adapter_PriceRule.OnLongItemClickListener longItemClickListener) {
             super(itemView);
             priceRule = itemView.findViewById(R.id.priceRule);
             price = itemView.findViewById(R.id.price);
             edit_btn = itemView.findViewById(R.id.editBtn);
+            checkbox = itemView.findViewById(R.id.priceRuleCheckBox);
+            background = itemView.findViewById(R.id.relativeLayout);
+            this.longItemClickListener = longItemClickListener;
+            itemView.setOnLongClickListener(this);
         }
+
+        @Override
+        public boolean onLongClick(View v) {
+            longItemClickListener.onLongItemClick(getAdapterPosition());
+            return true;
+        }
+    }
+
+    public interface OnLongItemClickListener {
+        void onLongItemClick(int position);
+    }
+
+    public interface OnSelectionChangedListener {
+        void onSelectionChanged(int count);
+    }
+
+    public void clearSelectedItems() {
+        selectedItems.clear();
     }
 }
