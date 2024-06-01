f8a08c230bfccc22d4db181da3041ab106b705ad
Angeline B <bedisad@students.nu-moa.edu.ph>
2024-06-02 01:49:33 +0800

    Added Delete Function: Rooms

--- app/src/main/java/com/example/nuradadmin/Activities/List_and_Create/Activity_Rooms.java
+++ app/src/main/java/com/example/nuradadmin/Activities/List_and_Create/Activity_Rooms.java
@@ -1,8 +1,13 @@
 package com.example.nuradadmin.Activities.List_and_Create;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,29 +23,36 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.nuradadmin.Activities.SideMenu.Activity_SystemManagement;
 import com.example.nuradadmin.Adapters.Adapter_Room;
-import com.example.nuradadmin.Adapters.Adapter_RoomType;
 import com.example.nuradadmin.Models.Model_Room;
 import com.example.nuradadmin.R;
 import com.example.nuradadmin.Utilities.SystemUIUtil;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Activity_Rooms extends AppCompatActivity  implements Adapter_RoomType.OnLongItemClickListener{
+public class Activity_Rooms extends AppCompatActivity implements Adapter_Room.OnLongItemClickListener, Adapter_Room.OnSelectionChangedListener {
     public static boolean isContextualModeEnabled = false;
+    private ImageView back_icon, moveOutDeleteMode, delete_icon;;
     private RecyclerView recyclerView;
-    private List<Model_Room> modelRoomList;
     private List<String> roomTitles;
-    private Adapter_Room adapter;
-    private ImageView back_icon;
-    private TextView title;
+    private List<Model_Room> modelRoomList;
+    private DatabaseReference room_DBref, recommended_DBref;
+    private TextView title, itemCounter;
     private FloatingActionButton floatingBtn;
+    private Adapter_Room adapter;
+    private View toolbar_normal;
+    private View toolbar_deleteMode;
+    @SuppressLint("NotifyDataSetChanged")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,14 +66,27 @@ public class Activity_Rooms extends AppCompatActivity  implements Adapter_RoomTy
         SystemUIUtil.setupSystemUI(this);
 
         back_icon = findViewById(R.id.back_icon);
+        moveOutDeleteMode = findViewById(R.id.back_btn);
         title = findViewById(R.id.title);
         floatingBtn = findViewById(R.id.floatingActionButton);
         recyclerView = findViewById(R.id.recyclerView);
+        toolbar_normal = findViewById(R.id.toolbar_normal);
+        toolbar_deleteMode = findViewById(R.id.toolbar_deleteMode);
+        itemCounter = findViewById(R.id.counter);
+        delete_icon = findViewById(R.id.delete_icon);
 
         modelRoomList = new ArrayList<>();
         roomTitles = new ArrayList<>();
 
         title.setText("Rooms");
+
+        room_DBref = FirebaseDatabase.getInstance().getReference("Rooms");
+        recommended_DBref = FirebaseDatabase.getInstance().getReference("Check");
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        adapter = new Adapter_Room(Activity_Rooms.this, modelRoomList, this, this);
+        recyclerView.setAdapter(adapter);
+        fetchRoomsFromDatabase( "Rooms", "Check");
 
         back_icon.setOnClickListener(View -> {
             Intent i = new Intent(this, Activity_SystemManagement.class);
@@ -75,19 +100,32 @@ public class Activity_Rooms extends AppCompatActivity  implements Adapter_RoomTy
             finish();
         });
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        adapter = new Adapter_Room(Activity_Rooms.this, modelRoomList);
-        recyclerView.setAdapter(adapter);
-        fetchRoomsFromDatabase( "Rooms", "Check");
-//        fetchRoomsFromDatabase("AllRooms", "RecommRooms", "Rooms");
+        moveOutDeleteMode.setOnClickListener(view -> {
+            isContextualModeEnabled = false;
+            toolbar_deleteMode.setVisibility(View.GONE);
+            toolbar_normal.setVisibility(View.VISIBLE);
+            adapter.clearSelectedItems();
+            adapter.notifyDataSetChanged();
+            updateItemCounter(0);
+        });
+
+        delete_icon.setOnClickListener(view -> {
+            List<Model_Room> selectedItems = adapter.getSelectedItems();
+            if (selectedItems.isEmpty()) {
+                Toast.makeText(this, "No item selected", Toast.LENGTH_SHORT).show();
+            } else {
+                showDeleteItemDialog(this);
+            }
+        });
     }
 
     private void fetchRoomsFromDatabase(String... referencePaths) {
-        modelRoomList.clear();
+        modelRoomList.clear(); // Clear the list before adding new data
         roomTitles.clear();
         for (String referencePath : referencePaths) {
             DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference(referencePath);
             databaseReference.addValueEventListener(new ValueEventListener() {
+                @SuppressLint("NotifyDataSetChanged")
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
                     for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
@@ -108,11 +146,116 @@ public class Activity_Rooms extends AppCompatActivity  implements Adapter_RoomTy
         }
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     @Override
     public void onLongItemClick(int position) {
-        // Handle long click action here
-        isContextualModeEnabled = true;
+        if (!adapter.getSelectedItems().contains(position)) {
+            isContextualModeEnabled = true;
+            updateItemCounter(1);
+            toolbar_normal.setVisibility(View.GONE);
+            toolbar_deleteMode.setVisibility(View.VISIBLE);
+            adapter.notifyDataSetChanged();
+        }
+    }
 
-        adapter.notifyDataSetChanged();
+    public void onSelectionChanged(int count) {
+        updateItemCounter(count);
+    }
+
+    private void updateItemCounter(int count) {
+        itemCounter.setText(count + " Item(s) Selected");
+    }
+
+    private void showDeleteItemDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Delete Item");
+        builder.setMessage("Are you sure you want to delete the selected item(s)?");
+
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Delete selected item(s)
+                for (Model_Room room : adapter.getSelectedItems()) {
+                    checkAndDeleteRoom(room);
+                }
+                // Reset contextual mode and update UI
+                isContextualModeEnabled = false;
+                toolbar_deleteMode.setVisibility(View.GONE);
+                toolbar_normal.setVisibility(View.VISIBLE);
+                adapter.clearSelectedItems();
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        builder.create().show();
+    }
+
+    private void checkAndDeleteRoom(Model_Room room) {
+        room_DBref.child(room.getRoomName()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                modelRoomList.clear(); // Clear the list before adding new data
+                roomTitles.clear();
+                if (snapshot.exists()) {
+                    room_DBref.child(room.getRoomName()).removeValue();
+                    deleteImageFromStorage(room.getImageUrl());
+                } else {
+                    recommended_DBref.child(room.getRoomName()).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if (snapshot.exists()) {
+                                recommended_DBref.child(room.getRoomName()).removeValue();
+                                deleteImageFromStorage(room.getImageUrl());
+                            } else {
+                                Log.e("Activity_Rooms", "Room does not exist in database (recommended): " + room.getRoomName());
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            // Handle database error
+                            Log.e("Activity_Rooms", "Database error (recommended): " + error.getMessage());
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle database error
+                Log.e("Activity_Rooms", "Database error: " + error.getMessage());
+            }
+        });
+    }
+
+    private void deleteImageFromStorage(String imageUrl) {
+        StorageReference storageRef = FirebaseStorage.getInstance().getReferenceFromUrl(imageUrl);
+        storageRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("Activity_Rooms", "Image deleted successfully");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("Activity_Rooms", "Error deleting image: " + e.getMessage());
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        isContextualModeEnabled = false;
+        if (adapter != null) {
+            adapter.notifyDataSetChanged();
+        }
     }
 }
--- app/src/main/java/com/example/nuradadmin/Adapters/Adapter_Room.java
+++ app/src/main/java/com/example/nuradadmin/Adapters/Adapter_Room.java
@@ -2,21 +2,26 @@ package com.example.nuradadmin.Adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
-import com.example.nuradadmin.Activities.List_and_Create.Activity_CreatePriceRules;
 import com.example.nuradadmin.Activities.List_and_Create.Activity_CreateRoom;
+import com.example.nuradadmin.Activities.List_and_Create.Activity_RoomTypes;
+import com.example.nuradadmin.Activities.List_and_Create.Activity_Rooms;
 import com.example.nuradadmin.Models.Model_PriceRule;
+import com.example.nuradadmin.Models.Model_RoomType;
 import com.example.nuradadmin.R;
 import com.example.nuradadmin.Models.Model_Room;
 import com.google.firebase.database.DataSnapshot;
@@ -26,7 +31,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
-import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
@@ -35,18 +40,25 @@ public class Adapter_Room extends RecyclerView.Adapter<Adapter_Room.RoomViewHold
     private DatabaseReference priceRules_DBref;
     private Context context;
     private List<Model_Room> roomList;
+    private Adapter_Room.OnLongItemClickListener longItemClickListener;
+    private Adapter_Room.OnSelectionChangedListener selectionChangedListener;
+    private List<Integer> selectedItems = new ArrayList<>();
 
-    public Adapter_Room(Context context, List<Model_Room> roomList) {
+    public Adapter_Room(Context context, List<Model_Room> roomList,
+                        Adapter_Room.OnLongItemClickListener longItemClickListener,
+                        Adapter_Room.OnSelectionChangedListener selectionChangedListener) {
         this.context = context;
         this.roomList = roomList;
         priceRules_DBref = FirebaseDatabase.getInstance().getReference("Price Rules");
+        this.longItemClickListener = longItemClickListener;
+        this.selectionChangedListener = selectionChangedListener;
     }
 
     @NonNull
     @Override
     public RoomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_room, parent, false);
-        return new RoomViewHolder(view);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_room, parent, false);
+        return new RoomViewHolder(view,longItemClickListener);
     }
 
     @Override
@@ -71,6 +83,65 @@ public class Adapter_Room extends RecyclerView.Adapter<Adapter_Room.RoomViewHold
         // Fetch price rule for the room
         fetchPriceRule(room.getPriceRule(), holder);
 
+        holder.checkbox.setOnCheckedChangeListener(null); // Clear previous listener
+
+        if (Activity_Rooms.isContextualModeEnabled && selectedItems.contains(position)) {
+            holder.checkbox.setChecked(true);
+            holder.background.setBackgroundColor(Color.parseColor("#FBE3F3"));
+        } else {
+            holder.checkbox.setChecked(false);
+            holder.background.setBackgroundColor(ContextCompat.getColor(context, R.color.white));
+        }
+
+        if (!Activity_Rooms.isContextualModeEnabled) {
+            // CONTEXTUAL MODE: FALSE
+            holder.checkbox.setVisibility(View.GONE);
+            holder.background.setOnClickListener(v -> {
+                holder.background.setBackgroundColor(ContextCompat.getColor(context, R.color.white));
+            });
+
+            holder.background.setOnLongClickListener(v -> {
+                if (!selectedItems.contains(position)) {
+                    // Add the item to selectedItems only if it's not already selected
+                    selectedItems.add(position);
+                    selectionChangedListener.onSelectionChanged(selectedItems.size());
+                }
+                Activity_RoomTypes.isContextualModeEnabled = true;
+                longItemClickListener.onLongItemClick(position);
+                notifyDataSetChanged();
+                return true;
+            });
+        } else {
+            // CONTEXTUAL MODE: TRUE
+            holder.checkbox.setVisibility(View.VISIBLE);
+            holder.background.setOnClickListener(v -> {
+                if (holder.checkbox.isChecked()) {
+                    selectedItems.remove((Integer) position);
+                } else {
+                    selectedItems.add(position);
+                }
+                selectionChangedListener.onSelectionChanged(selectedItems.size());
+                notifyDataSetChanged();
+            });
+
+            holder.background.setOnLongClickListener(v -> {
+                holder.background.setBackgroundColor(ContextCompat.getColor(context, R.color.white));
+                holder.checkbox.setChecked(false);
+                return false;
+            });
+        }
+
+        holder.checkbox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                selectedItems.add(position);
+                holder.background.setBackgroundColor(Color.parseColor("#FBE3F3"));
+            } else {
+                selectedItems.remove((Integer) position);
+                holder.background.setBackgroundColor(ContextCompat.getColor(context, R.color.white));
+            }
+            selectionChangedListener.onSelectionChanged(selectedItems.size());
+        });
+
         holder.editBtn.setOnClickListener(view ->{
             Intent intent = new Intent(context, Activity_CreateRoom.class);
             intent.putExtra("Purpose", "View Details");
@@ -89,6 +160,14 @@ public class Adapter_Room extends RecyclerView.Adapter<Adapter_Room.RoomViewHold
     @Override
     public int getItemCount() {
         return roomList.size();
+    }
+
+    public List<Model_Room> getSelectedItems() {
+        List<Model_Room> selectedRooms = new ArrayList<>();
+        for (int i : selectedItems) {
+            selectedRooms.add(roomList.get(i));
+        }
+        return selectedRooms;
     }
 
     private void fetchPriceRule(String priceRuleName, RoomViewHolder holder) {
@@ -144,7 +223,9 @@ public class Adapter_Room extends RecyclerView.Adapter<Adapter_Room.RoomViewHold
         notifyDataSetChanged();
     }
 
-    public static class RoomViewHolder extends RecyclerView.ViewHolder {
+    public static class RoomViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener {
+        CheckBox checkbox;
+        RelativeLayout background;
         ImageView roomImage;
         TextView roomTitle;
         TextView depositRequired;
@@ -152,8 +233,8 @@ public class Adapter_Room extends RecyclerView.Adapter<Adapter_Room.RoomViewHold
         TextView roomPrice;
         TextView priceDetails;
         Button editBtn;
-
-        public RoomViewHolder(@NonNull View itemView) {
+        Adapter_Room.OnLongItemClickListener longItemClickListener;
+        public RoomViewHolder(@NonNull View itemView, Adapter_Room.OnLongItemClickListener longItemClickListener) {
             super(itemView);
             roomImage = itemView.findViewById(R.id.roomImage);
             roomTitle = itemView.findViewById(R.id.roomTitle);
@@ -162,6 +243,28 @@ public class Adapter_Room extends RecyclerView.Adapter<Adapter_Room.RoomViewHold
             roomPrice = itemView.findViewById(R.id.roomPrice);
             priceDetails = itemView.findViewById(R.id.priceDetails);
             editBtn = itemView.findViewById(R.id.editBtn);
+            checkbox = itemView.findViewById(R.id.roomCheckBox);
+            background = itemView.findViewById(R.id.relativeLayout);
+            this.longItemClickListener = longItemClickListener;
+            itemView.setOnLongClickListener(this);
         }
+
+        @Override
+        public boolean onLongClick(View v) {
+            longItemClickListener.onLongItemClick(getAdapterPosition());
+            return true;
+        }
+    }
+
+    public interface OnLongItemClickListener {
+        void onLongItemClick(int position);
+    }
+
+    public interface OnSelectionChangedListener {
+        void onSelectionChanged(int count);
+    }
+
+    public void clearSelectedItems() {
+        selectedItems.clear();
     }
 }
