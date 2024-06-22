2f0185dc13aa865fa3849142bf3de76bd0ae4310
Angeline B <bedisad@students.nu-moa.edu.ph>
2024-06-23 00:12:57 +0800

    SplashScreen

---
+++ app/src/main/java/com/example/nuradadmin/Activities/Activity_SplashScreen.java
@@ -0,0 +1,48 @@
+package com.example.nuradadmin.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.nuradadmin.Activities.SideMenu.Activity_Dashboard;
+import com.example.nuradadmin.R;
+
+public class Activity_SplashScreen extends AppCompatActivity {
+    Animation rotateAnimation;
+    ImageView logo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        logo = findViewById(R.id.logo);
+        rotateAnimation();
+
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(Activity_SplashScreen.this, Activity_Dashboard.class);
+            startActivity(i);
+            finish();
+        }, 6000);
+    }
+
+    private void rotateAnimation() {
+        rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        logo.startAnimation(rotateAnimation);
+    }
+}
